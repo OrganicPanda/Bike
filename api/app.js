27d6d370a173f1b9d93eadb61fa8855abb9dd804
Steve Thomson <organicpanda@gmail.com>
2014-12-10 19:36:52 +0000

    Heroku host setup

--- api/app.js
+++ api/app.js
@@ -1,15 +1,15 @@
 var Promise = require('es6-promise').polyfill()
   , hapi = require('hapi')
   , routes = require('./lib/routes')
-  , port = process.env.PORT || 3000
-  , serverUrl = 'http://localhost:' + port;
+  , host = '0.0.0.0'
+  , port = +process.env.PORT || 3000;
 
-var server = new hapi.Server('localhost', port);
+var server = new hapi.Server(host, port);
 
 server.pack.register([{
   plugin: require('hapi-swagger'),
   options: {
-    basePath: serverUrl,
+    basePath: 'http://' + host + ':' + port,
     apiVersion: '1.0.0'
   }
 }], function(err) {
@@ -18,6 +18,6 @@ server.pack.register([{
   server.start(function(err) {
     if (err) return console.error('Couldn\'t start server!');
 
-    console.log('Server started at', serverUrl);
+    console.log('Server started at', server.info.uri);
   });
 });
